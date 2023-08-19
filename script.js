2d9058696b8b0e7b94d887f71633f94ebcab03d5
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-19 09:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const line = document.getElementById('line');
   }
 
   rotateLine();
+it("should have the correct line properties", () => {
+  cy.get('#line').should('have.css', 'position', 'absolute')
+    .and(line => {
+      const width = Math.round(parseFloat(line.css('width')));
+      expect(width).to.eq(200);
+    })
+    .and('have.css', 'height', '2px')
+    .and('have.css', 'background-color', 'rgb(0, 0, 0)');
+});
